d8e83d9b90029468d0886d8bb9a43dd7f314c995
Yahevin <Yahevin15@gmail.com>
2020-11-18 15:46:48 +0300

    screenshot testing

--- src/storyshot.test.js
+++ src/storyshot.test.js
@@ -1,3 +1,4 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots();
+initStoryshots({ suite: 'Puppeteer storyshots', test: imageSnapshot() });
