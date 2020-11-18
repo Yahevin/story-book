a132faf6269be8486ce3c3f1eda4c24850b4aca9
Yahevin <Yahevin15@gmail.com>
2020-11-18 15:18:07 +0300

    It works!

--- src/stories/Page.js
+++ src/stories/Page.js
@@ -59,13 +59,3 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
     </section>
   </article>
 );
-Page.propTypes = {
-  user: PropTypes.shape({}),
-  onLogin: PropTypes.func.isRequired,
-  onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
-
-Page.defaultProps = {
-  user: null,
-};
